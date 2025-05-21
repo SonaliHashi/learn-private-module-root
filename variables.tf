abea4ff8da368fade3c5aeabf4633467bc9c8a8b
SonaliHashi <sonali.jha@hashicorp.com>
2025-05-21 20:03:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
-  type        = us-east-1
+  type        = string
 }
 
 variable "prefix" {
   description = "This is the environment your webapp will be prefixed with. dev, qa, or prod."
-  type        = dev-test
+  type        = string
 }
 
 variable "name" {
   description = "Your name to attach to the webapp address."
-  type        = sonalijha
+  type        = string
 }
