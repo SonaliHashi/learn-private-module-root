2bbd06fa60cf50782190a84eb42ed5e5f6a6c6db
SonaliHashi <sonali.jha@hashicorp.com>
2025-05-21 19:13:57 +0530

    changes made in repo

--- main.tf
+++ main.tf
@@ -3,9 +3,6 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name    = var.name
-  region  = var.region
-  prefix  = var.prefix
+  source  = "app.terraform.io/sonali_hashi_tf/s3-webapp/aws"
   version = "1.0.0"
 }
